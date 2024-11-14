5314bbce91a863ab600490dfad961a66b4a1a191
dobot0101 <lhl890@naver.com>
2024-11-14 23:22:22 +0900

    feat: Member 엔티티에 @Getter 추가

--- src/main/java/com/dobot0101/springbootauth/domain/member/Member.java
+++ src/main/java/com/dobot0101/springbootauth/domain/member/Member.java
@@ -4,10 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "members")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
   @Id
@@ -18,9 +23,6 @@ public class Member {
   private String name;
   @Setter
   private String password;
-
-  protected Member() {
-  }
 
   public Member(UUID id, String email, String name, String password) {
     this.id = id;

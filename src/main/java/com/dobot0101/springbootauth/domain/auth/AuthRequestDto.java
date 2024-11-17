b3a5deb1f6431aa1882318b30a2df11578748cba
dobot0101 <lhl890@naver.com>
2024-11-17 19:17:05 +0900

    feat: dto validation 추가

--- src/main/java/com/dobot0101/springbootauth/domain/auth/AuthRequestDto.java
+++ src/main/java/com/dobot0101/springbootauth/domain/auth/AuthRequestDto.java
@@ -1,9 +1,18 @@
 package com.dobot0101.springbootauth.domain.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AuthRequestDto {
+
+  @NotBlank(message = "이메일을 입력해 주세요.")
+  @Email(message = "이메일 형식으로 입력해 주세요.")
   private String email;
+
+  @NotBlank(message = "암호를 입력해 주세요.")
+  @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
   private String password;
 }
--- src/main/java/com/dobot0101/springbootauth/domain/member/CreateMemberDto.java
+++ src/main/java/com/dobot0101/springbootauth/domain/member/CreateMemberDto.java
@@ -1,5 +1,8 @@
 package com.dobot0101.springbootauth.domain.member;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,7 +10,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public class CreateMemberDto {
 
+  @NotBlank(message = "이메일을 입력해 주세요.")
+  @Email(message = "이메일 형식으로 입력해 주세요.")
   private String email;
+  @NotBlank(message = "이름을 입력해 주세요.")
   private String name;
+  @NotBlank(message = "암호를 입력해 주세요.")
+  @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
   private String password;
 }
